f1adc93583554c653c32165080ed05f7b6e3687b
laggingreflex <laggingreflex@gmail.com>
2018-02-16 16:52:53 +0530

    fix: consistent indentation - spaces:4

--- src/ChocolateStore/PackageCacher.cs
+++ src/ChocolateStore/PackageCacher.cs
@@ -9,37 +9,37 @@ using HtmlAgilityPack;
 
 namespace ChocolateStore
 {
-	class PackageCacher
-	{
+    class PackageCacher
+    {
 
-		private const string INSTALL_FILE = "tools/chocolateyInstall.ps1";
+        private const string INSTALL_FILE = "tools/chocolateyInstall.ps1";
 
-		public delegate void FileHandler(string fileName);
-		public delegate void DownloadFailedHandler(string url, Exception ex);
+        public delegate void FileHandler(string fileName);
+        public delegate void DownloadFailedHandler(string url, Exception ex);
 
-		public event FileHandler SkippingFile = delegate { };
-		public event FileHandler DownloadingFile = delegate { };		
-		public event DownloadFailedHandler DownloadFailed = delegate { };
+        public event FileHandler SkippingFile = delegate { };
+        public event FileHandler DownloadingFile = delegate { };
+        public event DownloadFailedHandler DownloadFailed = delegate { };
 
-		public void CachePackage(Arguments arguments)
-		{
-			if (arguments.Url == null)
-			{
-				arguments.Url = GetPackagePath(arguments.PackageName);
-			}
+        public void CachePackage(Arguments arguments)
+        {
+            if (arguments.Url == null)
+            {
+                arguments.Url = GetPackagePath(arguments.PackageName);
+            }
 
-			var packagePath = DownloadFile(arguments.Url, arguments.Directory);
+            var packagePath = DownloadFile(arguments.Url, arguments.Directory);
 
-			using (var zip = ZipFile.Read(packagePath))
-			{
-				var entry = zip.FirstOrDefault(x => string.Equals(x.FileName, INSTALL_FILE, StringComparison.OrdinalIgnoreCase));
+            using (var zip = ZipFile.Read(packagePath))
+            {
+                var entry = zip.FirstOrDefault(x => string.Equals(x.FileName, INSTALL_FILE, StringComparison.OrdinalIgnoreCase));
 
-				if (entry != null) {
-					string content = null;
-					var packageName = Path.GetFileNameWithoutExtension(packagePath);
+                if (entry != null) {
+                    string content = null;
+                    var packageName = Path.GetFileNameWithoutExtension(packagePath);
 
-					using (MemoryStream ms = new MemoryStream()) {
-						entry.Extract(ms);
+                    using (MemoryStream ms = new MemoryStream()) {
+                        entry.Extract(ms);
                         ms.Position = 0;
                         using (StreamReader reader = new StreamReader(ms, true))
                         {
@@ -47,71 +47,71 @@ namespace ChocolateStore
                         }
                     }
 
-					content = CacheUrlFiles(Path.Combine(arguments.Directory, packageName), content);
-					zip.UpdateEntry(INSTALL_FILE, content);
-					zip.Save();
+                    content = CacheUrlFiles(Path.Combine(arguments.Directory, packageName), content);
+                    zip.UpdateEntry(INSTALL_FILE, content);
+                    zip.Save();
 
-				}
+                }
 
-			}
+            }
 
-		}
+        }
 
-		private string CacheUrlFiles(string folder, string content)
-		{
+        private string CacheUrlFiles(string folder, string content)
+        {
 
             const string pattern = "(?<=['\"])http[\\S ]*(?=['\"])";
 
-			if (!Directory.Exists(folder)) {
-				Directory.CreateDirectory(folder);
-			}
+            if (!Directory.Exists(folder)) {
+                Directory.CreateDirectory(folder);
+            }
 
-			return Regex.Replace(content, pattern, new MatchEvaluator(m => DownloadFile(m.Value, folder)));
+            return Regex.Replace(content, pattern, new MatchEvaluator(m => DownloadFile(m.Value, folder)));
 
-		}
+        }
 
-		private string GetPackagePath(string packageName)
-		{
-			var url = "https://chocolatey.org/packages/" + packageName;
-			var web = new HtmlWeb();
-			var doc = web.Load(url);
-			return doc.DocumentNode
-				.SelectSingleNode("//a[contains(@title, 'nupkg')]")
-				.Attributes["href"].Value;
-		}
+        private string GetPackagePath(string packageName)
+        {
+            var url = "https://chocolatey.org/packages/" + packageName;
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            return doc.DocumentNode
+                .SelectSingleNode("//a[contains(@title, 'nupkg')]")
+                .Attributes["href"].Value;
+        }
 
-		private string DownloadFile(string url, string destination)
-		{
+        private string DownloadFile(string url, string destination)
+        {
 
-			try
-			{
-				var request = WebRequest.Create(url);
-				var response = request.GetResponse();
-				var fileName = Path.GetFileName(response.ResponseUri.LocalPath);
-				var filePath = Path.Combine(destination, fileName);
+            try
+            {
+                var request = WebRequest.Create(url);
+                var response = request.GetResponse();
+                var fileName = Path.GetFileName(response.ResponseUri.LocalPath);
+                var filePath = Path.Combine(destination, fileName);
 
-				if (File.Exists(filePath))
-				{
-					SkippingFile(fileName);
-				}
-				else
-				{
-					DownloadingFile(fileName);
-					using (var fs = File.Create(filePath))
-					{
-						response.GetResponseStream().CopyTo(fs);
-					}
-				}
+                if (File.Exists(filePath))
+                {
+                    SkippingFile(fileName);
+                }
+                else
+                {
+                    DownloadingFile(fileName);
+                    using (var fs = File.Create(filePath))
+                    {
+                        response.GetResponseStream().CopyTo(fs);
+                    }
+                }
 
-				return filePath;
-			}
-			catch (Exception ex)
-			{
-				DownloadFailed(url, ex);
-				return url;
-			}
+                return filePath;
+            }
+            catch (Exception ex)
+            {
+                DownloadFailed(url, ex);
+                return url;
+            }
 
-		}
+        }
 
-	}
+    }
 }
