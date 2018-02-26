b2db5fb76996fc47431289c1131f9d1f3acff05a
Sebastian Rettig <sr258@users.noreply.github.com>
2018-02-26 08:56:26 +0100

    added $variable parsing and corresponding unit tests

---
+++ ChocolateStoreTest/ArgumentParserTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using ChocolateStore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChocolateStoreTest
+{
+    [TestClass]
+    public class ArgumentParserTests
+    {
+        [TestMethod]
+        public void NoVariables()
+        {
+            var arguments = ArgumentParser.ParseArguments(new[] { ".\\cache", "packagename" });
+            Assert.AreEqual(".\\cache", arguments.Directory);
+            Assert.AreEqual("packagename", arguments.PackageName);
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void NoPackageName()
+        {
+            ArgumentParser.ParseArguments(new[] { ".\\cache" });
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void NoArguments()
+        {
+            ArgumentParser.ParseArguments(new string[] { });
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void MalformedVariable()
+        {
+            ArgumentParser.ParseArguments(new[] { ".\\cache", "packagename", "variablevalue" });
+        }
+
+        [TestMethod]
+        public void OneSimpleVariable()
+        {
+            var arguments = ArgumentParser.ParseArguments(new[] { ".\\cache", "packagename", "$variable=value" });
+            Assert.AreEqual(1, arguments.Variables.Count());
+            Assert.AreEqual("variable", arguments.Variables.ElementAt(0).Item1);
+            Assert.AreEqual(1, arguments.Variables.ElementAt(0).Item2.Count());
+            Assert.AreEqual("value", arguments.Variables.ElementAt(0).Item2.ElementAt(0));
+        }
+
+        [TestMethod]
+        public void OneVariableWithAlternatives()
+        {
+            var arguments = ArgumentParser.ParseArguments(new[] { ".\\cache", "packagename", "$variable=value1|value2|value3" });
+            Assert.AreEqual(1, arguments.Variables.Count());
+            Assert.AreEqual("variable", arguments.Variables.ElementAt(0).Item1);
+            Assert.AreEqual(3, arguments.Variables.ElementAt(0).Item2.Count());
+            Assert.AreEqual("value1", arguments.Variables.ElementAt(0).Item2.ElementAt(0));
+            Assert.AreEqual("value2", arguments.Variables.ElementAt(0).Item2.ElementAt(1));
+            Assert.AreEqual("value3", arguments.Variables.ElementAt(0).Item2.ElementAt(2));
+        }
+
+        [TestMethod]
+        public void TwoSimpleVariables()
+        {
+            var arguments = ArgumentParser.ParseArguments(new[] { ".\\cache", "packagename", "$variable1=value1", "$variable2=value2" });
+            Assert.AreEqual(2, arguments.Variables.Count());
+            Assert.AreEqual("variable1", arguments.Variables.ElementAt(0).Item1);
+            Assert.AreEqual(1, arguments.Variables.ElementAt(0).Item2.Count());
+            Assert.AreEqual("value1", arguments.Variables.ElementAt(0).Item2.ElementAt(0));
+            Assert.AreEqual("variable2", arguments.Variables.ElementAt(1).Item1);
+            Assert.AreEqual(1, arguments.Variables.ElementAt(1).Item2.Count());
+            Assert.AreEqual("value2", arguments.Variables.ElementAt(1).Item2.ElementAt(0));
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void MalformedVariable2()
+        {
+            ArgumentParser.ParseArguments(new[] { ".\\cache", "packagename", "$variable=" });
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void MalformedVariable3()
+        {
+            ArgumentParser.ParseArguments(new[] { ".\\cache", "packagename", "=value" });
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void MalformedVariable4()
+        {
+            ArgumentParser.ParseArguments(new[] { ".\\cache", "packagename", "variable=value" });
+        }
+    }
+}
---
+++ ChocolateStoreTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChocolateStoreTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChocolateStoreTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf2acb45-9866-4481-a106-37299cfbd645")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
